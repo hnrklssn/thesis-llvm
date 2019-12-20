4466b310a886ef35bdd1c812d5be11a8d8a75f58
Henrik Olsson <hnrklssn@gmail.com>
2019-12-20 14:15:40 +0100

    Progress on synthesising variable names from metadata
    Can handle nested structs. Not all cases covered yet. Need to handle arrays.

--- test.c
+++ test.c
@@ -1,10 +1,21 @@
+struct MyStruct;
+struct Inner {
+	void *inner1;
+	struct MyStruct *inner2;
+};
 struct MyStruct {
 	int firstField;
-	void *secondField;
+	struct Inner secondField;
 };
 
 typedef struct MyStruct MyStruct;
 
 int my_fun(MyStruct s) {
-	return s.firstField;
+	int secret_int;
+	if(s.secondField.inner1) {
+		secret_int = s.secondField.inner2->firstField;
+	} else {
+		secret_int = 0;
+	}
+	return s.firstField + secret_int;
 }
